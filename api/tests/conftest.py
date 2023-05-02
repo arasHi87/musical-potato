1b8848618fcf4e29c85ef28b2b5881c72bb8b995
arashi87 <arasi27676271@gmail.com>
2023-05-02 16:51:13 +0800

    test(conftest): add fixture to create testing file

--- api/tests/conftest.py
+++ api/tests/conftest.py
@@ -1,11 +1,28 @@
+from pathlib import Path
 from typing import Generator
 
 import pytest
 from app import APP
 from fastapi.testclient import TestClient
+from tests import DEFAULT_FILE
 
 
 @pytest.fixture(scope="module")
 def client() -> Generator:
     with TestClient(APP) as tc:
         yield tc
+
+
+@pytest.fixture(scope="module")
+def file() -> Generator:
+    path = Path("/tmp") / DEFAULT_FILE.name
+
+    # delete file if exists
+    path.unlink(missing_ok=True)
+
+    # create file and write data
+    path.write_text(DEFAULT_FILE.content)
+
+    # yield file
+    with open(path, "rb") as fp:
+        yield fp
