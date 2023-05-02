15c34d0b9db02d4a3443df2e0428f1fd73cd90ea
arashi87 <arasi27676271@gmail.com>
2023-05-02 16:54:14 +0800

    test(__init__): add DEFAULT_FILE use to test and support files field in RequestBody

--- api/tests/__init__.py
+++ api/tests/__init__.py
@@ -1,15 +1,25 @@
 from dataclasses import dataclass
-from typing import Any, Callable, Dict, Mapping, Optional
+from typing import Any, BinaryIO, Callable, Dict, Tuple, Union
 
+import schemas
 from app import APP
 from fastapi.testclient import TestClient
 from starlette.datastructures import URLPath
+
+DEFAULT_FILE = schemas.File(
+    name="m3ow87.txt",
+    size=26,
+    checksum="d44d11c472f88a15737ae8eee2247231",
+    content="Do U Want To Meow With Me?",
+    content_type="text/plain",
+)
 
 
 @dataclass
 class RequestBody:
     url: URLPath
     body: Dict[str, Any]
+    files: Union[Dict[str, Tuple[str, BinaryIO]], None] = None
 
 
 @dataclass
@@ -30,7 +40,7 @@ class AssertRequest:
         **kwargs,
     ):
         url = APP.url_path_for(req_body.url)
-        resp = tc.request(method, url, json=req_body.body)
+        resp = tc.request(method, url, json=req_body.body, files=req_body.files)
 
         # If assert_func is not None, use assert_func to assert
         if assert_func is not None:
