523ef717aae63e7de445848e248b7862fd5ba0fb
arashi87 <arasi27676271@gmail.com>
2023-05-05 14:25:27 +0800

    docs: rename APP title and description

--- api/config.py
+++ api/config.py
@@ -3,12 +3,14 @@ from pydantic import BaseSettings
 
 class Settings(BaseSettings):
     class Config:
-        env_file = ".env"
+        env_file = "../.env"
         env_file_encoding = "utf-8"
 
     """Application configuration"""
-    APP_TITLE: str = "storage interface implement"
-    APP_DESCRIPTION: str = "Implement a simple RAID 5 storage web interface"
+    APP_TITLE: str = "RAID 3 Storage System"
+    APP_DESCRIPTION: str = (
+        "Implement a simple web interface for the RAID 3 storage system."
+    )
     APP_VERSION: str = "0.1.0"
     APP_OPENAPI_URL: str = "/openapi.json"
     APP_PREFIX: str = "/api"
